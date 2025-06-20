3c62baa11e5d54a94b64aec92d269b9503daf8d9
Abhi-96K <154230704+Abhi-96K@users.noreply.github.com>
2025-06-20 10:35:52 +0530

    Update settings.py

--- Store/Store/settings.py
+++ Store/Store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-^bgj2dtv-8n$c#&#qa6l4$)ud=j+fw$4cj^ygxu&nz9=!&fiqq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['your-app-name.onrender.com']
+ALLOWED_HOSTS = ['main.onrender.com']
 
 
 # Application definition
